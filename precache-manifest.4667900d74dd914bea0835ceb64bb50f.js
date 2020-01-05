8552ff98d6a29aedf2f75296da0758753fed0269
Vasyl Onufriienko <v.onufriienko@gmail.com>
2020-01-05 22:49:59 +0200

    Updates

---
+++ precache-manifest.4667900d74dd914bea0835ceb64bb50f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db89c988db70f1e0eea9683d03fa97b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "af6a94f02a068cc0de04",
+    "url": "/monsters-rolodex/static/css/main.05df46e5.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af6a94f02a068cc0de04",
+    "url": "/monsters-rolodex/static/js/main.81929801.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
